0a9bdf078c9e385c7562b11bfcd9ff4beb72262a
Alexandra DLG <adelagueronniere@gmail.com>
2018-04-26 12:03:13 +0200

    first app ok

---
+++ static_pages/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
+  get '/about/quentin', to: 'static_pages#quentin', as: 'super_man'
+  get '/about/alex', to: 'static_pages#alex', as: 'wonder_woman'
+end
