2f1a6b4bb8b7776384c36dbb984e76c8a627c112
Quentin <berson.quentin@gmail.com>
2018-04-26 16:29:34 +0200

    add username validation

--- app_with_backend/app/controllers/users_controller.rb
+++ app_with_backend/app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
     def new
-
+        @user = User.new
     end
 
     def create
-        @user = User.create(name: params["user_name"], bio: params["bio"])
-        redirect_to root_path
+        @user = User.new(name: params["user_name"], bio: params["bio"])
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render 'new'
+        end
+    end
+
+    def show 
+        @user = User.find(params[:id])
     end
 end
